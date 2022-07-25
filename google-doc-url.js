cdee3cba20dcf8e0f3ae94ef4f68941f58b52857
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-07-25 17:52:43 -0400

    Change from CSV to API
    removed chapters.csv and options.csv and added an API key for sheets

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MfVNAPik_NkAN65qJHIFoRtvTicKpdePMdZ7hoGdRKA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyB49xQTNQSVATYAihUGv72DUQkxRq9ySis';

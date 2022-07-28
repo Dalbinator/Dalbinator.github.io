40926abd788b5f12808e39b5dda84be01482cc29
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-07-27 21:20:17 -0400

    Update leaflet-providers.js

--- scripts/leaflet-providers.js
+++ scripts/leaflet-providers.js
@@ -152,11 +152,12 @@
 		
 		
 		Thunderforest: {
-			url: '//{s}.tile.thunderforest.com/{variant}/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
+			url: '//{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
 			options: {
 				attribution:
 					'&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, {attribution.OpenStreetMap}',
-				variant: 'cycle'
+					maxZoom: 19
+				//variant: 'cycle'
 			},
 			variants: {
 				OpenCycleMap: 'cycle',

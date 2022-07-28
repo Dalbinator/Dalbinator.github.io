2a9f36c7e231a81a931404684e1d974ab7e896ca
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-07-27 20:55:40 -0400

    Update leaflet-providers.js

--- scripts/leaflet-providers.js
+++ scripts/leaflet-providers.js
@@ -148,19 +148,19 @@
 				attribution: 'Map data: {attribution.OpenStreetMap}, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 			}
 		},
-		/* my sad attempt
+		/* my sad attempt*/
 		Thunderforest: {
 			url: 'https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
 			options: {
 				attribution:
-					'&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, {attribution.OpenStreetMap}',
+					'thunderforest',
 					maxZoom: 22
 				}
 			},
-		*/
-		/*original code: */
+		
+		/*original code: 
 		Thunderforest: {
-			url: 'http//{s}.tile.thunderforest.com/{variant}/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
+			url: '//{s}.tile.thunderforest.com/{variant}/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
 			options: {
 				attribution:
 					'&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, {attribution.OpenStreetMap}',
@@ -191,7 +191,7 @@
 				Pioneer: 'pioneer'
 			}
 		},
-		
+		*/
 		OpenMapSurfer: {
 			url: 'http://korona.geog.uni-heidelberg.de/tiles/{variant}/x={x}&y={y}&z={z}',
 			options: {

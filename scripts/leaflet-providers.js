56c20e9c1204435de563a46c7739570b5d816f70
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-08-02 21:43:01 -0400

    Update leaflet-providers.js

--- scripts/leaflet-providers.js
+++ scripts/leaflet-providers.js
@@ -263,7 +263,7 @@
 			}
 		},
 		MapTiler: {
-			url: 'https://api.maptiler.com/maps/{variant}/{z}/{x}/{y}{r}.png?key=K4a9knT0iEiGCzwTgiXu',
+			url: 'https://api.maptiler.com/maps/{variant}/{z}/{x}/{y}{r}.png?key=RgmWMtEWGuEwgCRAgKzt',
 			options: {
 				attribution:
 					'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',

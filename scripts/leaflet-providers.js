123f35f8abeedce4117870c639e9791399064f21
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-08-06 21:51:41 -0400

    Update leaflet-providers.js

--- scripts/leaflet-providers.js
+++ scripts/leaflet-providers.js
@@ -77,7 +77,7 @@
 
 	L.TileLayer.Provider.providers = {
 		OpenCycle: {
-			url: 'https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
+			url: 'https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=94df2c1a3f5143e9a142ca9523a8c3ca',
 			options: {
 				maxZoom: 19,
 				attribution: 'thank you'

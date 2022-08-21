e58ed966996950a7647db279aae13c9ec104518e
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-08-20 21:06:11 -0400

    adding ebc folder

---
+++ EBC/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MfVNAPik_NkAN65qJHIFoRtvTicKpdePMdZ7hoGdRKA/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyB49xQTNQSVATYAihUGv72DUQkxRq9ySis';

de606556d3979c5d77d5cda616e77d9d55b1aefa
Dalbinator <109922269+Dalbinator@users.noreply.github.com>
2022-09-26 19:03:44 -0400

    Update google-doc-url.js

--- EBC/google-doc-url.js
+++ EBC/google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQLyRTCqsBcfcW8i6hbWI9Vl737ln_q-3y4D5tFWx3eGy4vy2Y15rQgFtQ80sfvnBwM3Pjia_f95UBa/pubhtml;
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1N3UsyCTOhrc5hT-_8QyeULwiHqrTvKrWUuTRtNvQ67c/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyB49xQTNQSVATYAihUGv72DUQkxRq9ySis';
